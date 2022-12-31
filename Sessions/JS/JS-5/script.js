8b213747fccd8b2b35d20c47adc3e6294ed05839
ibrahimyav <ibrahimhakanyavuz@gmail.com>
2022-12-31 15:34:27 -0500

    JS Function

---
+++ Sessions/JS/JS-5/script.js
@@ -0,0 +1,48 @@
+// x=10;
+
+// console.log(x);
+
+// var x;
+
+// y=10
+// console.log(y);
+
+// let y; 
+
+// sayHi()
+
+// function sayHi(){
+//     console.log('Hello');
+// }
+
+// sayHello()
+
+// var sayHello = function(){
+//     console.log('Hello');
+// }
+
+// function greet(params) {
+//     console.log(`Hello ${params}`);
+// }
+
+// greet("John")
+// greet('Jane')
+
+
+// function number(){
+//     let numb1=+prompt('Sayi gir la');
+//     let numb2=+prompt('Bi daha gir la');
+//     console.log(numb1+numb2);
+// }
+
+// number();
+
+let add= function (num1,num2 ){
+    const result=num1+num2;
+    return result
+}
+
+// console.log(result);
+const res1= add(4,4);
+console.log(res1);
+console.log(add(5,10));
