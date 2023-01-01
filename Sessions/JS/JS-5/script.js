cd140f84a719253c52c5cec979d7543cf737578d
ibrahimyav <ibrahimhakanyavuz@gmail.com>
2023-01-01 02:10:18 -0500

    JS Function-1

--- Sessions/JS/JS-5/script.js
+++ Sessions/JS/JS-5/script.js
@@ -37,12 +37,54 @@
 
 // number();
 
-let add= function (num1,num2 ){
-    const result=num1+num2;
-    return result
+// let add= function (num1,num2 ){
+//     const result=num1+num2;
+//     return result
+// }
+
+// // console.log(result);
+// const res1= add(4,4);
+// console.log(res1);
+// console.log(add(5,10));
+
+
+
+// function reverse(word){
+//     let result="";
+//     for (let i = 0; i < word.length; i++) {
+//         result=word[i]+result;
+//     }
+//     return result
+// }
+
+// console.log(reverse(prompt('Enter your text')));
+
+
+// function fact(numb){
+//     let result=1;
+//     for (let i = 1; i <= numb; i++) {
+//         result=i*result;
+//     }
+//     return result
+// }
+
+// console.log(fact(+prompt('Enter your number')));
+
+// function greet(name){
+//     console.log('Hello', name);
+// }
+
+// greet("John")
+
+function greater(a,b,c){
+
+    if((a>b)&&(a>c))
+    {return a}
+    else if ((b>a)&&(b>c)){
+    return b
+    }else{
+        return c;
+    }
 }
 
-// console.log(result);
-const res1= add(4,4);
-console.log(res1);
-console.log(add(5,10));
+console.log(greater(+prompt("Enter a number"),+prompt("Enter a number"),+prompt("Enter a number")));
