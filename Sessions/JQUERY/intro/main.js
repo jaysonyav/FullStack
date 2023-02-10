ef6c446aac24d758b3e5e0d157f243203e76a28c
ibrahimyav <ibrahimhakanyavuz@gmail.com>
2023-02-09 19:36:06 -0500

    jQuerry Lesson 2.session

--- Sessions/JQUERY/intro/main.js
+++ Sessions/JQUERY/intro/main.js
@@ -4,6 +4,15 @@ console.log(firstHeader.textContent);
 
 console.log($('h1:first').text());
 
+console.log($('h1:nth-child(2)').text());
+
+$('h1:nth-child(2)').mouseenter(function () { 
+    $('h1:nth-child(2)').css('color','red')
+});
+$('h1:nth-child(2)').mouseout(function () { 
+    $('h1:nth-child(2)').css('color','black')
+});
+
 // $('...')
 // $('.className')
 // $('#id')
@@ -14,6 +23,10 @@ console.log($('h1:first').text());
 // $('#s').click(()=>{$('.thisOne').show(1000)});
 // $('#t').click(()=>{$('.thisOne').toggle(1000)});
 
-$('#h').click(()=>{$('.thisOne').fadeOut(1000)});
-$('#s').click(()=>{$('.thisOne').fadeIn(1000)});
-$('#t').click(()=>{$('.thisOne').fadeToggle(1000)});
+// $('#h').click(()=>{$('.thisOne').fadeOut(1000)});
+// $('#s').click(()=>{$('.thisOne').fadeIn(1000)});
+// $('#t').click(()=>{$('.thisOne').fadeToggle(1000)});
+
+$('#h').click(()=>{$('.thisOne').slideUp(1000)});
+$('#s').click(()=>{$('.thisOne').slideDown(1000)});
+$('#t').click(()=>{$('.thisOne').slideToggle(1000)});
