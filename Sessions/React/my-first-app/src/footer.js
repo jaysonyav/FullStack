c4ed742f7a2d7739c7752b8439dc048d41ff454a
ibrahimyav <ibrahimhakanyavuz@gmail.com>
2023-02-13 22:00:01 -0500

    React Session-2 End

---
+++ Sessions/React/my-first-app/src/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <h1>Canim Garim</h1>
+    )
+}
+
+
+export default Footer;
---
+++ Sessions/React/my-first-app/src/header.js
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+    <h1>This is header</h1>
+    )
+}
+
+export default Header;
