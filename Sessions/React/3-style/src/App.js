f7cb51c2cbf46720970404d77416bf38ad9a9cf6
ibrahimyav <ibrahimhakanyavuz@gmail.com>
2023-02-25 22:14:39 -0500

    React-3 Session-1 End

--- Sessions/React/3-style/src/App.js
+++ Sessions/React/3-style/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './footer'
 
 function App() {
+
+  const data="hello"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header greetngText={data}/>
+      <Main greetngText={data}/>
+      <Footer greetngText={data}/>
     </div>
   );
 }
 
+
+function Header(props){
+  console.log(props.greetngText,'under hedaer');
+  return(
+    <h1 style={{color:"red",fontFamily:"cursive", backgroundColor:"teal", border:'2px solid black',boxShadow:'5px 5px 5px grey'}}>I am the header of this page</h1>
+  )
+}
+
+function Main(props){
+  return(
+    <h2>I am the body of this page {props.greetngText}</h2>
+  )
+}
+
+
+
+
 export default App;
+
+let product1={name:'Iphone',year:2013}
---
+++ Sessions/React/3-style/src/footer.js
@@ -0,0 +1,4 @@
+const Footer =()=>
+  <h3>I am the footer this app</h3>
+
+export default Footer;
